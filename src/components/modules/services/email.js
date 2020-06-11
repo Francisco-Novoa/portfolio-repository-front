e7abf635ddb819cb318dcc48d3ae0ecad8eda5d5
Francisco Novoa Parada <francisco.novoa.14@sansano.usm.cl>
2020-06-11 01:22:05 -0400

    adding functionality to mail

--- src/components/modules/services/email.js
+++ src/components/modules/services/email.js
@@ -1,23 +1,34 @@
-import React,{useState} from "react"
+import React, { useState } from "react"
 import ButtonAction1, { ButtonAction2 } from "../../buttons/BottonesFuncionales"
+import Axios from "axios"
+import logger from "./logger"
+
 
 export default function SendMessage() {
-    const [local, setLocal]=useState({
-        name:"",
-        email:"",
-        message:""
+    const [local, setLocal] = useState({
+        name: "",
+        email: "",
+        message: ""
     })
-    const onChange =({target})=>{
-        setLocal({...local,[target.name]:target.value})
+    const onChange = ({ target }) => {
+        setLocal({ ...local, [target.name]: target.value })
     }
-    const onSubmit = () => {
-        console.log(local)
+    const onSubmit = async () => {
+        try {
+            const result = await Axios.post("https://panchoportafoliobackend.herokuapp.com/email", { sender: local.name, subject: local.email, body: local.message })
+            logger.info(result)
+        }
+        catch (error) {
+            console.dir(error)
+        }
+
     }
+
     const cleanup = () => {
         setLocal({
-            name:"",
-            email:"",
-            message:""
+            name: "",
+            email: "",
+            message: ""
         })
     }
     return (
@@ -26,13 +37,13 @@ export default function SendMessage() {
                 <form method="post" action="#">
                     <div className="row gtr-50">
                         <div className="col-6 col-12-small">
-                            <input type="text" name="name" value={local.name} onChange={(e)=>{onChange(e)}} id="contact-name" placeholder="Nombre" />
+                            <input type="text" name="name" value={local.name} onChange={(e) => { onChange(e) }} id="contact-name" placeholder="Nombre" />
                         </div>
                         <div className="col-6 col-12-small">
-                            <input type="text" name="email" value={local.email} onChange={(e)=>{onChange(e)}} id="contact-email" placeholder="Email" />
+                            <input type="text" name="email" value={local.email} onChange={(e) => { onChange(e) }} id="contact-email" placeholder="Email" />
                         </div>
                         <div className="col-12">
-                            <textarea name="message" id="contact-message" value={local.message} onChange={(e)=>{onChange(e)}} placeholder="Mensaje" rows="4"></textarea>
+                            <textarea name="message" id="contact-message" value={local.message} onChange={(e) => { onChange(e) }} placeholder="Mensaje" rows="4"></textarea>
                         </div>
                         <div className="col-12">
                             <ul className="actions">
